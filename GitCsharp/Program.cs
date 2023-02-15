24a7d72e42547d07e95d41ffc408e9034499bd9e
The-Magpie-7 <themagpie99@gmail.com>
2023-02-15 16:02:53 -0600

    adding the list

--- GitCsharp/Program.cs
+++ GitCsharp/Program.cs
@@ -1,12 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // 
-// List and loop through
-
-
-
-
-
+// List and loop through - Dana
+List<string> petsList = new List<string>() {"Buddy", "Bean", "Sunny", "Sergeant Linq"};
+//loop through items in list and print them out
+foreach(string pet in petsList){
+    Console.WriteLine($"This is my pet, {pet}.");
+}
 
 
 
