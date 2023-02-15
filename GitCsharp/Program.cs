5c90bd32f0990d6cc1e303c65db9964f7cb7f10a
murielfossi <mina78aaa@gmail.com>
2023-02-15 16:02:45 -0600

    Dictionary part

--- GitCsharp/Program.cs
+++ GitCsharp/Program.cs
@@ -23,3 +23,16 @@ Console.WriteLine("Hello, World!");
 
 
 // Dictionary and loop through
+
+Dictionary<string,int> grades=new Dictionary<string, int>();
+grades.Add("Muriel",90 );
+grades.Add("Emily",95);
+grades.Add("Dana",96 );
+grades.Add("enzo",98);
+grades.Add("samy",99);
+
+for (int k = 0; k < grades.Count; k++)
+        {KeyValuePair<string, int> items=grades.ElementAt(k);
+            
+         Console.WriteLine($"{items.Key}:{items.Value}");
+        }
