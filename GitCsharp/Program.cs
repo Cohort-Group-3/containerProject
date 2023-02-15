99f635be1c5dec3f13f92ce213df9d80f1b17981
Emily Vazquez <emily.m.vazquez99@gmail.com>
2023-02-15 15:39:55 -0600

    created csharp file

---
+++ GitCsharp/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// 
+// List and loop through
+
+
+
+
+
+
+
+
+
+
+// Array and loop through
+
+
+
+
+
+
+
+
+
+// Dictionary and loop through
